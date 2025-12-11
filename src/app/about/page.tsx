be66ebf77abe88b92b504f4ff9e06486ce88e127
Adidya Abimanyu <114373205+AdidyaAbimanyu@users.noreply.github.com>
2025-12-11 23:48:20 +0700

    update projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Metadata } from "next";
 import FadeIn from "@/components/animations/FadeIn";
 import StaggerChildren from "@/components/animations/StaggerChildren";
 import { Card } from "@/components/ui/card";
